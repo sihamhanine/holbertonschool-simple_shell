182576845427b108da4382ac2bf18a1e676562d2
sihamelani <siham.elani17@gmail.com>
2024-04-24 20:51:48 +0000

    check erreur in read_input

--- main_shell.c
+++ main_shell.c
@@ -14,20 +14,27 @@ int main(int argc, char **argv, char **env)
     size_t input_size = 0;
     ssize_t n_char = 0;
     int status_return = 1;
+    int exit_num = -1;
+    int exit_status = EXIT_SUCCESS;
     (void)argc;
     while (status_return && n_char != EOF)
       {
+	input = NULL;
 	status_return = isatty(STDIN_FILENO);
 	if (status_return)
     {
       printf("Cisfun$ ");
     }
-      n_char = getline(&input, &input_size, stdin);
-      if (n_char == -1)
-	{
-	  perror("erreur lors de lecteur de l'entree\n");
-	  exit(EXIT_FAILURE);
-	}
+      if (read_input(&input, &input_size) == -1)
+		{
+			free(input);
+			break;
+		}
+      if (input[0] == '\0')
+		{
+			free(input);
+			continue;
+		}
       if (check_spaces_tabs(input))
     {
       free(input);
@@ -42,24 +49,28 @@ int main(int argc, char **argv, char **env)
       free_token_command(args);
       continue;
     }
-  if (strcmp(args[0], "exit") == 0)
-    {
-      free_token_command(args);
-      free(command);
-      free(input);
-      exit(EXIT_SUCCESS);
-    }
-  else if (strcmp(args[0], "env") == 0)
+  
+        if (strcmp(args[0], "exit") == 0)
         {
-print_env();
+                if (args[1])
+                        exit_status = atoi(args[1]);
+                else if (exit_num != -1)
+                        exit_status = exit_num;
                 free_token_command(args);
-                free(command);
-		free(input);
-		continue;                
+                free(input);
+                exit(exit_status);
         }
-  else {
+        else if (strcmp(args[0], "env") == 0)
+        {
+                print_env();
+                free_token_command(args);
+                continue;
+        }
+  else
+    {
+ 
           execute_command(argv, args, env);
-  }
+    }
   
   free(command);
   free_token_command(args);	  

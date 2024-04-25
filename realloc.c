85a67ec0444398fc022abb0ca7180d5da751fa25
Souf <soufianmghirat@hotmail.com>
2024-04-25 15:04:42 +0200

    fix betty

--- realloc.c
+++ realloc.c
@@ -1,10 +1,9 @@
 #include "shell.h"
 /**
- * realloc - Reallocate a memory block
+ * my_realloc - Reallocate a memory block
  * @ptr: Pointer to array
  * @old_size: Old size
  * @new_size: New size
- * 
  * Return: A pointer to the allocated memory
  */
 void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -13,12 +12,9 @@ void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *ptr1;
 	unsigned int i;
 
-	
 	ptr1 = (char *)ptr;
-	
 	if (ptr == NULL)
 		return (malloc(new_size));
-	
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -26,20 +22,14 @@ void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size == old_size)
 		return (ptr);
-
 	s = malloc((new_size) * sizeof(char));
 	if (s == NULL)
-	  {
-	    free(s);
+	{
+		free(s);
 		return (NULL);
 	}
-      
-       for (i = 0; i < old_size && i < new_size; i++)
-	 {
-        s[i] = ptr1[i];
-          }
-
+		for (i = 0; i < old_size && i < new_size; i++)
+			s[i] = ptr1[i];
 	free(ptr1);
 	return (s);
 }
-

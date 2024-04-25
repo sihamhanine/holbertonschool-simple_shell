4b0d48d09f0ba653c1ead0e8f3d1cd98fa1546ca
Souf <soufianmghirat@hotmail.com>
2024-04-25 16:50:14 +0200

    betty fix

--- realloc.c
+++ realloc.c
@@ -15,7 +15,10 @@ void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 		return (malloc(new_size));
 	if (new_size == 0 && ptr != NULL)
-		free(ptr), return (NULL);
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size == old_size)
 		return (ptr);
 	s = malloc((new_size) * sizeof(char));
